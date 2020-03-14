fab88963b456ed8d8ecc87cf165133d7c0944096
vpoletaev11 <vpoletaev11@gmail.com>
2020-03-14 03:55:06 +0300

    Improved readability. Tests simplified

--- heap/heap.go
+++ heap/heap.go
@@ -1,96 +1,102 @@
 package heap
 
 // Heap - binary tree where:
-// 1) The value at any vertex is no less than the values ​​of its descendants;
+// 1) The value at any parent node is no less than the values ​​of its children;
 // 2) The depth of all leaves (distance to the root) differs by no more than 1 layer;
 // 3) The last layer is filled from left to right without gaps.
 type Heap struct {
-	items []int
+	elements []int
 }
 
 // Len returns length of the heap
 func (h *Heap) Len() int {
-	return len(h.items)
+	return len(h.elements)
 }
 
 // Push appends element into Heap
-func (h *Heap) Push(x int) {
-	h.items = append(h.items, x)
+func (h *Heap) Push(element int) {
+	h.elements = append(h.elements, element)
 
-	indexPushedElem := h.Len() - 1
-	parentIndex := indexParent(indexPushedElem)
+	indexPushedElem := h.Len() - 1              // Get index of the last inserted element
+	parentIndex := indexParent(indexPushedElem) // Get index of the parent of the last inserted element
 
 	for {
-		if h.items[parentIndex] >= x {
+		if h.elements[parentIndex] >= element {
 			return
 		}
 
-		h.items[indexPushedElem], h.items[parentIndex] = h.items[parentIndex], h.items[indexPushedElem]
-
+		h.swap(indexPushedElem, parentIndex)
 		indexPushedElem = parentIndex
 		parentIndex = indexParent(indexPushedElem)
 	}
 }
 
 // Pop obtains and deletes root element from heap
-func (h *Heap) Pop() (int, bool) {
+func (h *Heap) Pop() (element int, status bool) {
 	if h.Len() == 0 {
 		return 0, false
 	}
 
-	elem := h.items[0]
-	h.items[0] = h.items[h.Len()-1]
-	h.items = h.items[:h.Len()-1]
+	element = h.elements[0]               // Obtain the root element
+	h.elements[0] = h.elements[h.Len()-1] // Set the last node in place of the root node
+	h.elements = h.elements[:h.Len()-1]   // Remove the last node
 
 	h.heapify(0)
 
-	return elem, true
+	return element, true
 }
 
 // Peek obtains element from heap
-func (h *Heap) Peek() (int, bool) {
+func (h *Heap) Peek() (element int, status bool) {
 	if h.Len() == 0 {
 		return 0, false
 	}
 
-	return h.items[0], true
+	return h.elements[0], true
 }
 
+// indexParent returns parent node index for inserted node index
 func indexParent(i int) int {
 	return (i - 1) / 2
 }
 
-func (h *Heap) indexChildLeft(rootIndex int) (childIndex int, exist bool) {
-	childIndex = 2*rootIndex + 1
+// indexChildLeft returns index of the left child for inserted parent index
+func (h *Heap) indexChildLeft(parentIndex int) (childIndex int, exist bool) {
+	childIndex = 2*parentIndex + 1
 	if childIndex > h.Len()-1 {
 		return 0, false
 	}
 	return childIndex, true
 }
 
-func (h *Heap) indexChildRight(rootIndex int) (childIndex int, exist bool) {
-	childIndex = 2*rootIndex + 2
+// indexChildRight returns index of the right child for inserted parent index
+func (h *Heap) indexChildRight(parentIndex int) (childIndex int, exist bool) {
+	childIndex = 2*parentIndex + 2
 	if childIndex > h.Len()-1 {
 		return 0, false
 	}
 	return childIndex, true
 }
 
+// swap swaps tho nodes
 func (h *Heap) swap(index1, index2 int) {
-	h.items[index1], h.items[index2] = h.items[index2], h.items[index1]
+	h.elements[index1], h.elements[index2] = h.elements[index2], h.elements[index1]
 }
 
+// heapify restores the property of ordering in the entire subtree, the root of which is the rootIndex
 func (h *Heap) heapify(rootIndex int) {
 	largest := rootIndex
+
 	indexLeftChild, exist := h.indexChildLeft(rootIndex)
-	if exist && (h.items[indexLeftChild] > h.items[largest]) {
+	if exist && (h.elements[indexLeftChild] > h.elements[largest]) {
 		largest = indexLeftChild
 	}
 
 	indexRightChild, exist := h.indexChildRight(rootIndex)
-	if exist && (h.items[indexRightChild] > h.items[largest]) {
+	if exist && (h.elements[indexRightChild] > h.elements[largest]) {
 		largest = indexRightChild
 	}
+
 	if rootIndex != largest {
 		h.swap(rootIndex, largest)
 		h.heapify(largest)
--- heap/heap_test.go
+++ heap/heap_test.go
@@ -1,7 +1,6 @@
 package heap
 
 import (
-	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -15,9 +14,7 @@ func TestLen(t *testing.T) {
 	heap.Push(3)
 	heap.Push(4)
 
-	if heap.Len() != 4 {
-		t.Error("Pushed 4 elements, but length of heap ==", heap.Len())
-	}
+	assert.Equal(t, 4, heap.Len())
 }
 
 func TestPush(t *testing.T) {
@@ -32,15 +29,6 @@ func TestPush(t *testing.T) {
 	heap.Push(99)
 	heap.Push(13)
 
-	expected := []int{5521, 13, 111, 4, 3, 2, 99, 1}
-	assert.Equal(t, expected, heap.items)
-}
-
-func TestPopSuccess(t *testing.T) {
-	var heap Heap
-	heap.items = []int{5521, 13, 111, 4, 3, 2, 99, 1}
-
-	expected := []int{5521, 111, 99, 13, 4, 3, 2, 1}
 	actual := []int{}
 	for heap.Len() > 0 {
 		elem, ok := heap.Pop()
@@ -48,6 +36,28 @@ func TestPopSuccess(t *testing.T) {
 			actual = append(actual, elem)
 		}
 	}
+
+	expected := []int{5521, 111, 99, 13, 4, 3, 2, 1}
+	assert.Equal(t, expected, actual)
+}
+
+func TestPopSuccess(t *testing.T) {
+	var heap Heap
+
+	heap.Push(1)
+	heap.Push(2)
+	heap.Push(3)
+	heap.Push(4)
+
+	expected := []int{4, 3, 2, 1}
+	actual := []int{}
+	for heap.Len() > 0 {
+		elem, ok := heap.Pop()
+		if ok {
+			actual = append(actual, elem)
+		}
+	}
+
 	assert.Equal(t, expected, actual)
 }
 
@@ -55,18 +65,14 @@ func TestPopEmptyHeap(t *testing.T) {
 	var heap Heap
 
 	elem, ok := heap.Pop()
-	if elem != 0 {
-		t.Error("Nothing pushed, but popped value ==", elem)
-	}
-	if ok != false {
-		t.Error("Nothing pushed, but pop status == true")
-	}
+	assert.Equal(t, 0, elem)
+	assert.False(t, ok)
 }
 
 func TestPeekSuccess(t *testing.T) {
 	var heap Heap
 
-	heap.items = []int{1}
+	heap.Push(1)
 
 	elem, ok := heap.Peek()
 	assert.Equal(t, 1, elem)
@@ -79,121 +85,4 @@ func TestPeekEmptyHeap(t *testing.T) {
 	elem, ok := heap.Peek()
 	assert.Equal(t, 0, elem)
 	assert.Equal(t, false, ok)
-}
-
-func TestIndexParent(t *testing.T) {
-	var heap Heap
-
-	heap.items = []int{5521, 13, 111, 4, 3, 2, 99, 1}
-
-	parent := indexParent(1)
-	assert.Equal(t, 0, parent)
-
-	parent = indexParent(3)
-	assert.Equal(t, 1, parent)
-
-	parent = indexParent(5)
-	assert.Equal(t, 2, parent)
-
-	parent = indexParent(7)
-	assert.Equal(t, 3, parent)
-}
-
-func TestIndexChildLeft(t *testing.T) {
-	var heap Heap
-
-	heap.items = []int{5521, 13, 111, 4, 3, 2, 99, 1}
-
-	leftChildIndex, exist := heap.indexChildLeft(0)
-	assert.Equal(t, 1, leftChildIndex)
-	assert.Equal(t, true, exist)
-
-	leftChildIndex, exist = heap.indexChildLeft(1)
-	assert.Equal(t, 3, leftChildIndex)
-	assert.Equal(t, true, exist)
-
-	leftChildIndex, exist = heap.indexChildLeft(7)
-	assert.Equal(t, 0, leftChildIndex)
-	assert.Equal(t, false, exist)
-}
-
-func TestIndexChildRight(t *testing.T) {
-	var heap Heap
-
-	heap.items = []int{5521, 13, 111, 4, 3, 2, 99, 1}
-
-	rightChildIndex, exist := heap.indexChildRight(0)
-	assert.Equal(t, 2, rightChildIndex)
-	assert.Equal(t, true, exist)
-
-	rightChildIndex, exist = heap.indexChildRight(1)
-	assert.Equal(t, 4, rightChildIndex)
-	assert.Equal(t, true, exist)
-
-	rightChildIndex, exist = heap.indexChildRight(7)
-	assert.Equal(t, 0, rightChildIndex)
-	assert.Equal(t, false, exist)
-}
-
-func TestSwap(t *testing.T) {
-	var heap Heap
-	heap.items = []int{1, 2}
-
-	heap.swap(0, 1)
-
-	expected := []int{2, 1}
-	assert.Equal(t, expected, heap.items)
-}
-
-func TestHeapify(t *testing.T) {
-	var heap Heap
-	// testing heapifying left child
-	heap.items = []int{4, 11, 9}
-
-	heap.heapify(0)
-
-	expected := []int{11, 4, 9}
-	assert.Equal(t, expected, heap.items)
-
-	// testing heapifying right child
-	heap.items = []int{4, 3, 11}
-
-	heap.heapify(0)
-
-	expected = []int{11, 3, 4}
-	assert.Equal(t, expected, heap.items)
-
-	// testing recursively heapifying
-	heap.items = []int{4, 11, 9, 10, 5, 6, 8, 1, 2, 4}
-
-	heap.heapify(0)
-
-	expected = []int{11, 10, 9, 4, 5, 6, 8, 1, 2, 4}
-	assert.Equal(t, expected, heap.items)
-}
-
-// TestHeapDemo displays example of using the heap data structure
-func TestHeapDemo(t *testing.T) {
-	fmt.Println("Demonstration of using the heap data structure:")
-	var heap Heap
-
-	fmt.Println("Pushed: 1")
-	heap.Push(1)
-
-	fmt.Println("Pushed: 8")
-	heap.Push(8)
-
-	fmt.Println("Pushed: 2")
-	heap.Push(2)
-
-	fmt.Println("Pushed: 10")
-	heap.Push(10)
-	fmt.Print("\n")
-
-	for heap.Len() > 0 {
-		elem, ok := heap.Pop()
-		if ok {
-			fmt.Println("Popped:", elem)
-		}
-	}
 }
