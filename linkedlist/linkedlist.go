2b2af2f2646eeac7896beeffb9df5d04b648de47
vpoletaev11 <vpoletaev11@gmail.com>
2020-03-18 03:46:00 +0300

    Linked list done

--- linkedlist/linkedlist.go
+++ linkedlist/linkedlist.go
@@ -4,14 +4,14 @@ import "fmt"
 
 // node of linked list
 type node struct {
-	data     string // stored data
-	nextNode *node  // pointer to next node
+	data     string // Stored data
+	nextNode *node  // Pointer to next node
 }
 
 // LinkedList -  data structure consisting of a collection of nodes,
-// where every node contain pointer to next node, which together represent a sequence
+// where every node contain pointer to next node, which together represent a sequence.
 type LinkedList struct {
-	head *node // first element in linked list
+	head *node // First element in linked list
 }
 
 // InsertHead adds value in begin of list
@@ -25,26 +25,22 @@ func (l *LinkedList) InsertHead(val string) {
 }
 
 // Insert adds value in list by index.
-// If on index already exists value, it and all subsequent will be moved by 1, and in his place will be inserted value
+// If on index already exists value, it and all subsequent values will be moved by 1, and in its place will be inserted new value
 func (l *LinkedList) Insert(index int, val string) (err error) {
 	if index < 0 {
 		return fmt.Errorf("Cannot use negative index: %v", index)
 	}
 
 	if index == 0 {
-		if l.head == nil {
-			l.head = &node{val, nil}
-		}
-
-		l.head = &node{val, l.head}
+		l.InsertHead(val)
+		return nil
 	}
 
-	prevNode := l.nodeByIndex(index - 1)
-	if prevNode == nil || prevNode.nextNode == nil {
+	prevNode := l.nodeByIndex(index - 1) // Find node before of candidate node to be inserted
+	if prevNode == nil {
 		return fmt.Errorf("Index out of range")
 	}
-
-	prevNode.nextNode = &node{val, prevNode.nextNode}
+	prevNode.nextNode = &node{val, prevNode.nextNode} // prevNode now pointing on inserted node which pointing to node who was here before
 
 	return nil
 }
@@ -72,31 +68,30 @@ func (l *LinkedList) GetHead() (val string, status bool) {
 }
 
 // Get obtains and deletes element by index from list
-func (l *LinkedList) Get(index int) (val string, status bool) {
+func (l *LinkedList) Get(index int) (val string, err error) {
 	if l.head == nil {
-		return "", false
+		return "", fmt.Errorf("Empty list")
 	}
 
 	if index < 0 {
-		return "", false
+		return "", fmt.Errorf("Cannot use negative index: %v", index)
 	}
 
 	if index == 0 {
+		// Similar to GetHead, but without check for empty of list
 		val = l.head.data
 		l.head = l.head.nextNode
-		return val, true
+		return val, nil
 	}
 
-	prevNode := l.nodeByIndex(index - 1)
+	prevNode := l.nodeByIndex(index - 1) // Find node before of candidate node to be obtained and deleted
 	if prevNode == nil || prevNode.nextNode == nil {
-		return "", false
+		return "", fmt.Errorf("Index out of range")
 	}
+	val = prevNode.nextNode.data                   // Obtain node data
+	prevNode.nextNode = prevNode.nextNode.nextNode // prevNode now pointing to next node after deleted node.
 
-	val = prevNode.nextNode.data
-
-	prevNode.nextNode = prevNode.nextNode.nextNode
-
-	return val, true
+	return val, nil
 }
 
 // GetTail obtains and deletes element from end of list
@@ -105,7 +100,7 @@ func (l *LinkedList) GetTail() (val string, status bool) {
 		return "", false
 	}
 
-	p1, p2 := l.head, l.head
+	p1, p2 := l.head, l.head // Initialize variables that stores pointers to the penultimate and last nodes respectively
 	for {
 		if p2.nextNode == nil {
 			break
@@ -114,9 +109,8 @@ func (l *LinkedList) GetTail() (val string, status bool) {
 		p1 = p2
 		p2 = p2.nextNode
 	}
-
 	val = p2.data
-	p1.nextNode = nil
+	p1.nextNode = nil // Penultimate node points to nil i.e now it last node
 
 	return val, true
 }
@@ -131,17 +125,17 @@ func (l LinkedList) PeekHead() (val string, status bool) {
 }
 
 // Peek obtains element by index from list
-func (l LinkedList) Peek(index int) (val string, status bool) {
+func (l LinkedList) Peek(index int) (val string, err error) {
 	if l.head == nil {
-		return "", false
+		return "", fmt.Errorf("Empty list")
 	}
 
 	node := l.nodeByIndex(index)
 	if node == nil {
-		return "", false
+		return "", fmt.Errorf("Index out of range")
 	}
 
-	return node.data, true
+	return node.data, nil
 }
 
 // PeekTail obtains element from end of list
@@ -174,7 +168,7 @@ func (l *LinkedList) nodeByIndex(index int) (pointer *node) {
 	}
 
 	for i := 0; i < index; i++ {
-		if pointer.nextNode == nil {
+		if pointer.nextNode == nil { // Check if last node founded before of index node
 			return nil
 		}
 		pointer = pointer.nextNode
--- linkedlist/linkedlist_test.go
+++ linkedlist/linkedlist_test.go
@@ -1,11 +1,13 @@
 package linkedlist
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
+// test InsertHead
 func TestInsertHead(t *testing.T) {
 	var l LinkedList
 
@@ -26,6 +28,64 @@ func TestInsertHead(t *testing.T) {
 	assert.True(t, ok)
 }
 
+// test Insert
+func TestInsertSuccess(t *testing.T) {
+	var l LinkedList
+
+	err := l.Insert(0, "two")
+	assert.Nil(t, err)
+
+	err = l.Insert(0, "one")
+	assert.Nil(t, err)
+
+	err = l.Insert(1, "three")
+	assert.Nil(t, err)
+
+	err = l.Insert(3, "four")
+	assert.Nil(t, err)
+
+	val, ok := l.GetHead()
+	assert.Equal(t, "one", val)
+	assert.True(t, ok)
+
+	val, ok = l.GetHead()
+	assert.Equal(t, "three", val)
+	assert.True(t, ok)
+
+	val, ok = l.GetHead()
+	assert.Equal(t, "two", val)
+	assert.True(t, ok)
+
+	val, ok = l.GetHead()
+	assert.Equal(t, "four", val)
+	assert.True(t, ok)
+}
+
+func TestInsertOutOfRange(t *testing.T) {
+	var l LinkedList
+
+	l.InsertHead("one")
+
+	err := l.Insert(5, "two")
+	assert.Equal(t, fmt.Errorf("Index out of range"), err)
+}
+
+func TestInsertOutOfRangeEmptyList(t *testing.T) {
+	var l LinkedList
+
+	err := l.Insert(5, "one")
+	assert.Equal(t, fmt.Errorf("Index out of range"), err)
+}
+
+func TestInsertNegativeIndex(t *testing.T) {
+	var l LinkedList
+
+	err := l.Insert(-1, "value")
+
+	assert.Equal(t, fmt.Errorf("Cannot use negative index: -1"), err)
+}
+
+// test InsertTail
 func TestInsertTail(t *testing.T) {
 	var l LinkedList
 
@@ -46,15 +106,16 @@ func TestInsertTail(t *testing.T) {
 	assert.True(t, ok)
 }
 
+// test GetHead
 func TestGetHeadSuccess(t *testing.T) {
 	var l LinkedList
 
-	l.InsertTail("one")
-	l.InsertTail("two")
-	l.InsertTail("three")
+	l.InsertHead("one")
+	l.InsertHead("two")
+	l.InsertHead("three")
 
 	val, ok := l.GetHead()
-	assert.Equal(t, "one", val)
+	assert.Equal(t, "three", val)
 	assert.True(t, ok)
 
 	val, ok = l.GetHead()
@@ -62,7 +123,7 @@ func TestGetHeadSuccess(t *testing.T) {
 	assert.True(t, ok)
 
 	val, ok = l.GetHead()
-	assert.Equal(t, "three", val)
+	assert.Equal(t, "one", val)
 	assert.True(t, ok)
 }
 
@@ -74,15 +135,65 @@ func TestGetHeadEmptyList(t *testing.T) {
 	assert.False(t, ok)
 }
 
+// test Get
+func TestGetSuccess(t *testing.T) {
+	var l LinkedList
+
+	l.InsertHead("one")
+	l.InsertHead("two")
+	l.InsertHead("three")
+
+	val, err := l.Get(2)
+	assert.Nil(t, err)
+	assert.Equal(t, "one", val)
+
+	val, err = l.Get(0)
+	assert.Nil(t, err)
+	assert.Equal(t, "three", val)
+
+	val, err = l.Get(0)
+	assert.Nil(t, err)
+	assert.Equal(t, "two", val)
+}
+
+func TestGetEmptyList(t *testing.T) {
+	var l LinkedList
+
+	val, err := l.Get(0)
+	assert.Equal(t, fmt.Errorf("Empty list"), err)
+	assert.Equal(t, "", val)
+}
+
+func TestGetNegativeIndex(t *testing.T) {
+	var l LinkedList
+
+	l.InsertHead("one")
+
+	val, err := l.Get(-1)
+	assert.Equal(t, fmt.Errorf("Cannot use negative index: -1"), err)
+	assert.Equal(t, "", val)
+}
+
+func TestGetOutOfRange(t *testing.T) {
+	var l LinkedList
+
+	l.InsertHead("one")
+
+	val, err := l.Get(5)
+	assert.Equal(t, fmt.Errorf("Index out of range"), err)
+	assert.Equal(t, "", val)
+}
+
+// test GetTail
 func TestGetTailSuccess(t *testing.T) {
 	var l LinkedList
 
-	l.InsertTail("one")
-	l.InsertTail("two")
-	l.InsertTail("three")
+	l.InsertHead("one")
+	l.InsertHead("two")
+	l.InsertHead("three")
 
 	val, ok := l.GetTail()
-	assert.Equal(t, "three", val)
+	assert.Equal(t, "one", val)
 	assert.True(t, ok)
 
 	val, ok = l.GetTail()
@@ -90,7 +201,7 @@ func TestGetTailSuccess(t *testing.T) {
 	assert.True(t, ok)
 
 	val, ok = l.GetTail()
-	assert.Equal(t, "one", val)
+	assert.Equal(t, "three", val)
 	assert.True(t, ok)
 }
 
@@ -102,6 +213,7 @@ func TestGetTailEmptyList(t *testing.T) {
 	assert.False(t, ok)
 }
 
+// test PeekHead
 func TestPeekHeadSuccess(t *testing.T) {
 	var l LinkedList
 
@@ -122,6 +234,38 @@ func TestPeekHeadEmptyList(t *testing.T) {
 	assert.False(t, ok)
 }
 
+// test Peek
+func TestPeekSuccess(t *testing.T) {
+	var l LinkedList
+
+	l.InsertHead("one")
+	l.InsertHead("two")
+	l.InsertHead("three")
+
+	val, err := l.Peek(2)
+	assert.Equal(t, "one", val)
+	assert.Nil(t, err)
+}
+
+func TestPeekEmptyList(t *testing.T) {
+	var l LinkedList
+
+	val, err := l.Peek(0)
+	assert.Equal(t, "", val)
+	assert.Equal(t, fmt.Errorf("Empty list"), err)
+}
+
+func TestPeekOutOfRange(t *testing.T) {
+	var l LinkedList
+
+	l.InsertHead("one")
+
+	val, err := l.Peek(2)
+	assert.Equal(t, "", val)
+	assert.Equal(t, fmt.Errorf("Index out of range"), err)
+}
+
+// test PeekTail
 func TestPeekTailSuccess(t *testing.T) {
 	var l LinkedList
 
