25409a38e997330adbdd1d168f1142ed785c1e6f
vpoletaev11 <vpoletaev11@gmail.com>
2020-03-29 03:58:28 +0300

    Tests shortened

--- stack/stack_test.go
+++ stack/stack_test.go
@@ -8,139 +8,95 @@ import (
 )
 
 // Testing slice based stack:
-//
-// test Len
-func TestLenSliceStack(t *testing.T) {
+func TestSliceStack(t *testing.T) {
 	var s stack.SliceStack
 
+	// Push
 	s.Push("one")
 	s.Push("two")
 	s.Push("three")
 
+	// Len
 	assert.Equal(t, 3, s.Len())
-}
 
-// test Push
-func TestPushSliceStack(t *testing.T) {
-	var s stack.SliceStack
-
-	s.Push("one")
-
-	actual, ok := s.Peek()
-	assert.Equal(t, "one", actual)
-	assert.True(t, ok)
-}
-
-// test Pop
-func TestPopSliceStackSuccess(t *testing.T) {
-	var s stack.SliceStack
-
-	s.Push("one")
-	s.Push("two")
-
-	elem, ok := s.Pop()
-	assert.Equal(t, "two", elem)
+	// Success Peek and Pop
+	val, ok := s.Peek()
+	assert.Equal(t, "three", val)
 	assert.True(t, ok)
 
-	elem, ok = s.Pop()
-	assert.Equal(t, "one", elem)
+	val, ok = s.Pop()
+	assert.Equal(t, "three", val)
 	assert.True(t, ok)
-}
 
-func TestPopEmptyStackSliceStack(t *testing.T) {
-	var s stack.SliceStack
+	val, ok = s.Peek()
+	assert.Equal(t, "two", val)
+	assert.True(t, ok)
 
-	elem, ok := s.Pop()
+	val, ok = s.Pop()
+	assert.Equal(t, "two", val)
+	assert.True(t, ok)
 
-	assert.Equal(t, "", elem)
+	val, ok = s.Peek()
+	assert.Equal(t, "one", val)
+	assert.True(t, ok)
+
+	val, ok = s.Pop()
+	assert.Equal(t, "one", val)
+	assert.True(t, ok)
+
+	// Peek and Pop in empty stack
+	val, ok = s.Peek()
+	assert.Equal(t, "", val)
+	assert.False(t, ok)
+
+	val, ok = s.Pop()
+	assert.Equal(t, "", val)
 	assert.False(t, ok)
 }
 
-// test Peek
-func TestPeekSliceStackSuccess(t *testing.T) {
-	var s stack.SliceStack
-
-	s.Push("one")
-	elem, ok := s.Peek()
-
-	assert.Equal(t, "one", elem)
-	assert.True(t, ok)
-}
-
-func TestPeekEmptyStackSliceStack(t *testing.T) {
-	var s stack.SliceStack
-
-	elem, ok := s.Peek()
-
-	assert.Equal(t, "", elem)
-	assert.False(t, ok)
-}
-
-// Testing list based stack:
-//
-// test Len
-func TestLenListStack(t *testing.T) {
+// Testing singly linked list based stack:
+func TestListStack(t *testing.T) {
 	var s stack.ListStack
 
+	// Push
 	s.Push("one")
 	s.Push("two")
 	s.Push("three")
 
+	// Len
 	assert.Equal(t, 3, s.Len())
-}
 
-// test Push
-func TestPushListStack(t *testing.T) {
-	var s stack.ListStack
-
-	s.Push("one")
-
-	actual, ok := s.Peek()
-	assert.Equal(t, "one", actual)
-	assert.True(t, ok)
-}
-
-// test Pop
-func TestPopListStackSuccess(t *testing.T) {
-	var s stack.ListStack
-
-	s.Push("one")
-	s.Push("two")
-
-	elem, ok := s.Pop()
-	assert.Equal(t, "two", elem)
+	// Success Peek and Pop
+	val, ok := s.Peek()
+	assert.Equal(t, "three", val)
 	assert.True(t, ok)
 
-	elem, ok = s.Pop()
-	assert.Equal(t, "one", elem)
+	val, ok = s.Pop()
+	assert.Equal(t, "three", val)
 	assert.True(t, ok)
-}
 
-func TestPopEmptyStackListStack(t *testing.T) {
-	var s stack.ListStack
+	val, ok = s.Peek()
+	assert.Equal(t, "two", val)
+	assert.True(t, ok)
 
-	elem, ok := s.Pop()
+	val, ok = s.Pop()
+	assert.Equal(t, "two", val)
+	assert.True(t, ok)
 
-	assert.Equal(t, "", elem)
+	val, ok = s.Peek()
+	assert.Equal(t, "one", val)
+	assert.True(t, ok)
+
+	val, ok = s.Pop()
+	assert.Equal(t, "one", val)
+	assert.True(t, ok)
+
+	// Peek and Pop in empty stack
+	val, ok = s.Peek()
+	assert.Equal(t, "", val)
 	assert.False(t, ok)
-}
 
-// test Peek
-func TestPeekListStackSuccess(t *testing.T) {
-	var s stack.ListStack
-
-	s.Push("one")
-	elem, ok := s.Peek()
-
-	assert.Equal(t, "one", elem)
-	assert.True(t, ok)
-}
-
-func TestPeekEmptyStackListStack(t *testing.T) {
-	var s stack.ListStack
-
-	elem, ok := s.Peek()
-
-	assert.Equal(t, "", elem)
+	val, ok = s.Pop()
+	assert.Equal(t, "", val)
 	assert.False(t, ok)
 }
