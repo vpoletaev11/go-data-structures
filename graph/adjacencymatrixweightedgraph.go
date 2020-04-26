396b8d2ae8020a128026a324940654fe88fa7894
vpoletaev11 <vpoletaev11@gmail.com>
2020-04-26 21:04:53 +0300

    AdjacencyMatrixGraph done

---
+++ graph/adjacencymatrixweightedgraph.go
@@ -0,0 +1,65 @@
+package graph
+
+// AdjacencyMatrixWeightedGraph - Weighted Graph based on adjacency matrix.
+// Quantity of rows or columns of the matrix == quantity of graph vertexes.
+// Matrix stores edges of graph vertexes.
+// Indexing of vertexes starts from 0.
+type AdjacencyMatrixWeightedGraph struct {
+	adjacencyMatrix [][]int
+}
+
+// Size returns count of vertexes in the graph
+func (a *AdjacencyMatrixWeightedGraph) Size() int {
+	return len(a.adjacencyMatrix)
+}
+
+// AdjMatrixWeightedGraphInit initializes AdjacencyMatrixDirectedGraph
+func AdjMatrixWeightedGraphInit(size int) *AdjacencyMatrixWeightedGraph {
+	if size < 1 {
+		return nil
+	}
+	var a AdjacencyMatrixWeightedGraph
+
+	a.adjacencyMatrix = make([][]int, size)
+	for i := 0; i < len(a.adjacencyMatrix); i++ {
+		a.adjacencyMatrix[i] = make([]int, size)
+	}
+
+	return &a
+}
+
+// AddEdge adds edge between vertexes
+func (a *AdjacencyMatrixWeightedGraph) AddEdge(from, to, weight int) bool {
+	if from > a.Size() || to > a.Size() || from < 0 || to < 0 {
+		return false
+	}
+
+	a.adjacencyMatrix[from][to] = weight
+	a.adjacencyMatrix[to][from] = weight
+	return true
+}
+
+// RemoveEdge removes edge between vertexes
+func (a *AdjacencyMatrixWeightedGraph) RemoveEdge(from, to int) bool {
+	if from > a.Size() || to > a.Size() || from < 0 || to < 0 {
+		return false
+	}
+
+	a.adjacencyMatrix[from][to] = 0
+	a.adjacencyMatrix[to][from] = 0
+	return true
+}
+
+// PeekEdges returns slice of pairs edges and edges weight of inputted vertex
+func (a *AdjacencyMatrixWeightedGraph) PeekEdges(vertex int) (edges [][]int, ok bool) {
+	if vertex > a.Size() || vertex < 0 {
+		return [][]int{}, false
+	}
+
+	for i, val := range a.adjacencyMatrix[vertex] {
+		if val != 0 {
+			edges = append(edges, []int{i, val})
+		}
+	}
+	return edges, true
+}
--- graph/graph_test.go
+++ graph/graph_test.go
@@ -226,6 +226,79 @@ func TestAdjacencyMatrixGraphOutOfRange(t *testing.T) {
 	assert.False(t, ok)
 }
 
+// Testing AdjacencyMatrixWeightedGraph:
+func TestAdjacencyMatrixWeightedGraphSUCCESS(t *testing.T) {
+	a := graph.AdjMatrixWeightedGraphInit(5)
+
+	dataset := []fromToWeight{{0, 0, 0}, {0, 1, 100}, {0, 2, 13}, {0, 3, 1000}, {0, 4, 20}, {1, 3, 1}, {4, 2, 8}, {3, 1, 14}, {2, 0, 75}}
+
+	assert.Equal(t, 5, a.Size())
+
+	for _, value := range dataset {
+		a.AddEdge(value.from, value.to, value.weight)
+	}
+
+	edges, ok := a.PeekEdges(0)
+	assert.Equal(t, [][]int{[]int{1, 100}, []int{2, 75}, []int{3, 1000}, []int{4, 20}}, edges)
+	assert.True(t, ok)
+
+	edges, ok = a.PeekEdges(1)
+	assert.Equal(t, [][]int{[]int{0, 100}, []int{3, 14}}, edges)
+	assert.True(t, ok)
+
+	edges, ok = a.PeekEdges(2)
+	assert.Equal(t, [][]int{[]int{0, 75}, []int{4, 8}}, edges)
+	assert.True(t, ok)
+
+	edges, ok = a.PeekEdges(3)
+	assert.Equal(t, [][]int{[]int{0, 1000}, []int{1, 14}}, edges)
+	assert.True(t, ok)
+
+	edges, ok = a.PeekEdges(4)
+	assert.Equal(t, [][]int{[]int{0, 20}, []int{2, 8}}, edges)
+	assert.True(t, ok)
+
+	// remove all edges
+	for _, value := range dataset {
+		a.RemoveEdge(value.from, value.to)
+	}
+
+	// check that graph haven't edges
+	for i := 0; i < a.Size(); i++ {
+		edges, ok = a.PeekEdges(i)
+		assert.Equal(t, [][]int(nil), edges)
+		assert.True(t, ok)
+	}
+}
+
+func TestAdjacencyMatrixWeightedGraphOutOfRange(t *testing.T) {
+	a := graph.AdjMatrixWeightedGraphInit(5)
+
+	// AdjMatrixDirectedWeightedGraphInit out of range
+	assert.Nil(t, graph.AdjMatrixWeightedGraphInit(0))
+
+	// AddEdge out of range
+	assert.False(t, a.AddEdge(-1, 0, 0))
+	assert.False(t, a.AddEdge(0, -1, 0))
+	assert.False(t, a.AddEdge(10, 0, 0))
+	assert.False(t, a.AddEdge(0, 10, 0))
+
+	// RemoveEdge out of range
+	assert.False(t, a.RemoveEdge(-1, 0))
+	assert.False(t, a.RemoveEdge(0, -1))
+	assert.False(t, a.RemoveEdge(10, 0))
+	assert.False(t, a.RemoveEdge(0, 10))
+
+	// PeekEdges out of range
+	edges, ok := a.PeekEdges(-1)
+	assert.Equal(t, [][]int{}, edges)
+	assert.False(t, ok)
+
+	edges, ok = a.PeekEdges(10)
+	assert.Equal(t, [][]int{}, edges)
+	assert.False(t, ok)
+}
+
 type fromToWeight struct {
 	from, to, weight int
 }
