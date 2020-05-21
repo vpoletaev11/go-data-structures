0019e63cfd1e75838cc820043a933f82ecd4098a
vpoletaev11 <vpoletaev11@gmail.com>
2020-05-22 00:07:36 +0300

    List queue rewritted. Enqueue now O(1)

--- queue/listqueue.go
+++ queue/listqueue.go
@@ -1,37 +1,66 @@
 package queue
 
-import "github.com/vpoletaev11/go-data-structures/list"
-
 /*
 	LIST QUEUE:
 
 	Len()                Enqueue()            Dequeue()            Peek()
-	Best:    O(n)        Best:    O(n)        Best:    O(1)        Best:    O(1)
-	Average: O(n)        Average: O(n)        Average: O(1)        Average: O(1)
-	Worst:   O(n)        Worst:   O(n)        Worst:   O(1)        Worst:   O(1)
+	Best:    O(n)        Best:    O(1)        Best:    O(1)        Best:    O(1)
+	Average: O(n)        Average: O(1)        Average: O(1)        Average: O(1)
+	Worst:   O(n)        Worst:   O(1)        Worst:   O(1)        Worst:   O(1)
 */
+
+type nodeLQ struct {
+	data     string  // Stored data
+	nextNode *nodeLQ // Pointer to next node
+}
 
 // ListQueue - singly linked list based list of elements organized according to the FIFO principle
 type ListQueue struct {
-	elements list.SinglyLinkedList
+	head *nodeLQ // First element in queue
+	tail *nodeLQ // Last element in queue
 }
 
 // Len returns length of the queue
 func (l *ListQueue) Len() int {
-	return l.elements.Len()
+	node := l.head
+	len := 1
+	for {
+		if node.nextNode == nil {
+			return len
+		}
+		node = node.nextNode
+		len++
+	}
 }
 
 // Enqueue adds element into queue
 func (l *ListQueue) Enqueue(val string) {
-	l.elements.InsertTail(val)
+	if l.tail == nil {
+		l.tail = &nodeLQ{val, nil}
+		l.head = l.tail
+		return
+	}
+
+	l.tail.nextNode = &nodeLQ{val, nil}
+	l.tail = l.tail.nextNode
 }
 
 // Dequeue obtains and deletes element from queue
 func (l *ListQueue) Dequeue() (val string, ok bool) {
-	return l.elements.GetHead()
+	if l.head == nil {
+		return "", false
+	}
+
+	val = l.head.data
+	l.head = l.head.nextNode
+	return val, true
 }
 
 // Peek obtains element from queue
 func (l *ListQueue) Peek() (val string, ok bool) {
-	return l.elements.PeekHead()
+	if l.head == nil {
+		return "", false
+	}
+
+	return l.head.data, true
 }
